e22e3299e5a2d9393d32e7bf0d174502d9bb4e2c
cesarledres <cesarledrescv@gmail.com>
2026-03-29 23:30:20 -0300

    exercicios de 1 ao 5 resolvidos

--- repo-java/exercicios-java-basico/src/Main.java
+++ repo-java/exercicios-java-basico/src/Main.java
@@ -2,7 +2,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        exerc1();
+        //exerc1();
+        //exerc2();
+        //exerc3();
+        //exerc4();
+        //exerc5();
     }
 
     public static void exerc1() {
@@ -12,8 +16,115 @@ public class Main {
 
         //Definindo apelido para scanner
         Scanner sc = new Scanner(System.in);
+
+        //print + input
         System.out.println("Digite seu nome: ");
-        
+        String nome = sc.nextLine();
+
+        //print final
+        System.out.println("Olá " + nome);
+
+        sc.close();
+    }
+
+    public static void exerc2() {
+        /*
+        Crie um programa que solicite dois valores numéricos. O mesmo deverá
+calcular e exibir:
+a. A soma dos dois valores
+b. A multiplicação dos dois valores
+c. A subtração dos dois valores
+d. A divisão dos dois valores
+        */
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número: ");
+        int num1 = sc.nextInt();
+
+        System.out.println("Digite o segundo número: ");
+        int num2 = sc.nextInt();
+
+        System.out.println("A soma dos dois números é " + (num1 + num2));
+        System.out.println("A subtração dos dois números é " + (num1 - num2));
+        System.out.println("A multiplicação dos dois números é " + (num1 * num2));
+        System.out.println("A divisão dos dois números é " + (num1 / num2));
+    }
+
+    public static void exerc3() {
+        /*
+        Crie um programa que solicite o peso e altura de uma determinada
+pessoa e calcule o seu IMC (Índice de Massa Corpórea). A fórmula: IMC = peso / altura * altura
+        */
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informe seu peso: ");
+        double peso = sc.nextDouble();
+
+        System.out.println("Informe sua altura: ");
+        double altura = sc.nextDouble();
+
+        double alturaQuadrado = Math.pow(altura, 2);
+        System.out.println(alturaQuadrado);
+
+        double imc = peso / alturaQuadrado;
+
+        System.out.printf("O IMC é %.2f", imc);
+
+        sc.close();
+    }
+
+    public static void exerc4() {
+        /*
+        Crie um programa que receba o salário de um funcionário e calcule o
+novo salário com um aumento de 15%. 
+        */
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informe o salário: ");
+        double salario = sc.nextDouble();
+
+        double salarioNovo = salario * 1.15;
+
+        System.out.printf("O sálario reajustado com 15%% é de R$ %.2f", salarioNovo);
+
+        sc.close();
+    }
+    
+    public static void exerc5() {
+        /*
+        Uma loja paga aos seus vendedores:
+a. Salário base de R$ 1.800,00
+b. Comissão fixa de R$ 150,00 por produto vendido
+c. 3% sobre o valor total das vendas
+Escreva um programa que solicite:
+• Nome do vendedor
+• Quantidade de produtos vendidos
+• Valor total das vendas
+O programa deve calcular e exibir o salário final.
+        */
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informe o seu nome: ");
+        String nome = sc.nextLine();
+
+        System.out.println("Informe a quantidade de produtos vendidos: ");
+        int qtdVendido = sc.nextInt();
+
+        System.out.println("Informe o valor total das vendas: ");
+        double valorVendas = sc.nextDouble();
+
+        double salarioBase = 1800;
+        double valorPorVendido = 150 * qtdVendido;
+        double PercTotalVendas = 0.03 * valorVendas;
+
+        double salarioTotal = salarioBase + valorPorVendido + PercTotalVendas;
+
+        System.out.printf("O salário total de é de R$ %.2f ", salarioTotal);
+
 
     }
 }
